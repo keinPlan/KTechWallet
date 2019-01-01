0fef24749eec10405bee766179fab931aab46e7a
keinPlan <4256502+keinPlan@users.noreply.github.com>
2019-01-01 15:35:03 +0100

    cordova

--- vue.config.js
+++ vue.config.js
@@ -13,7 +13,7 @@ module.exports = {
       filename: 'index.html',
       // when using title option,
       // template title tag needs to be <title><%= htmlWebpackPlugin.options.title %></title>
-      title: 'Index Page',
+      title: 'KTechWallet',
       // chunks to include on this page, by default includes
       // extracted common chunks and vendor chunks.
       chunks: ['chunk-vendors', 'chunk-common', 'index']
@@ -21,7 +21,7 @@ module.exports = {
 
   },
 
-  baseUrl: '/KTechWallet/',
+  baseUrl: process.env.BaseURL ? process.env.BaseURL :'/KTechWallet/',
   outputDir: undefined,
   assetsDir: undefined,
   runtimeCompiler: undefined,
