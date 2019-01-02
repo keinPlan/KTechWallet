519b366752b1b4069ce85ffb13d7e6e21012ed3c
keinPlan <4256502+keinPlan@users.noreply.github.com>
2019-01-02 19:39:48 +0100

    .

--- src/KTechWallet/App.vue
+++ src/KTechWallet/App.vue
@@ -94,7 +94,7 @@ export default class App extends Vue {
     if ("serviceWorker" in navigator) {
     
       window.addEventListener("load", function() {
-        navigator.serviceWorker.register("/sw.js", { scope: '/KTechWallet/' }).then(
+        navigator.serviceWorker.register("/sw.js").then(
           function(registration) {
             // Registration was successful
             console.log(
